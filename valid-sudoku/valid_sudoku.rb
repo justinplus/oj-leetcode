b6e9ea61bd070c6827a86119e5a4020f09813c6d
Justin Jia <justinj656@live.cn>
2015-07-23 23:26:02 +0800

    simplify valid_sudolu.rb

--- valid-sudoku/valid_sudoku.rb
+++ valid-sudoku/valid_sudoku.rb
@@ -6,14 +6,9 @@ Blank = '.'
 def is_valid_sudoku(board)  
   for i in Range.new(0, 8)
     # valid ith row
-    row = board[i].reject{ |item| item == Blank }
-    unique = row.uniq
-    return false unless unique == row
-    
+    return false unless board[i].reject{ |item| item == Blank }.uniq!.nil?
     # valid ith col
-    col = board.map { |r| r[i] }
-    unique = col.reject!{ |item| item == Blank }.uniq
-    return false unless unique == col
+    return false unless board.map { |r| r[i] }.reject{ |item| item == Blank }.uniq!.nil?
   end
 
   for i in 0..2
@@ -25,11 +20,9 @@ def is_valid_sudoku(board)
           block << board[i*3+bi][j*3+bj]
         end
       end
-      unique = block.reject!{ |item| item == Blank }.uniq
-      return false unless unique == block
+      return false unless block.reject{ |item| item == Blank }.uniq!.nil?
     end
   end
 
   true
 end
-
