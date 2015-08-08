9bc9aff285d0890a89e92a67c7c0bac4d6daffdb
Justin Jia <justinj656@live.cn>
2015-08-08 15:28:45 +0800

    AC: add_and_search_word.cpp, TODO: need destructor

--- add-and-search-word/add_and_search_word.cpp
+++ add-and-search-word/add_and_search_word.cpp
@@ -5,47 +5,55 @@
 using namespace std;
 
 class WordDictionary {
+  class TrieNode {
+    public:
+    TrieNode ** childs;
+    bool isWord; // Need initialization
+    // char c; No char is needed, index implicates this info.
+    TrieNode() : isWord(false) {
+      childs = new TrieNode * [26];
+      memset( childs, 0, sizeof(TrieNode *) * 26 );
+    }
+  };
+
   public:
-    static const int LEN = 'z' - 'a' + 1;
+  WordDictionary() : root(new TrieNode) {}
 
-    ~WordDictionary(){
-      for(unsigned i = 0; i < elems.size(); i++) {
-        delete [] elems[i];
-      }
+  // TODO: need destructor
+  // ~WordDictionary() {}
+
+  // Adds a word into the data structure.
+  void addWord(string word) {
+    TrieNode * cur = root;
+    for(unsigned i = 0; i < word.size(); i++){
+      int index = word[i] - 'a';
+      if(!cur->childs[index]) cur->childs[index] = new TrieNode(); // Notice: if !cur->childs[index]
+      cur = cur->childs[index];
     }
+    cur->isWord = true;
+  }
 
-    // Adds a word into the data structure.
-    void addWord(string word) {
-      for( unsigned i = elems.size(); i < word.size(); i++){
-        elems.push_back(NULL);
-      }
-      int pos = word.size() - 1;
-      if( elems[pos] == NULL ){
-        elems[pos] = new int[ LEN * word.size() ];
-        memset(elems[pos], 0, sizeof(int) * LEN * word.size());
-      }
-      for( unsigned i = 0; i < word.size(); i++){
-        elems[pos][ i*LEN + word[i] - 'a' ] = 1;
-      }
+  // Returns if the word is in the data structure. A word could
+  // contain the dot character '.' to represent any one letter.
+  bool search(string word) {
+    return search(word.c_str(), root);
+  }
 
+  bool search(const char * ch, const TrieNode * cur) {
+    if(!cur) return false;
+    if(*ch == '\0') return cur->isWord;
+    if(*ch == '.'){
+      for( int i = 0; i < 26; i++ ){
+        if( search(ch+1, cur->childs[i]) ) return true;
+      }      
+      return false;
+    } else {
+      return search(ch+1, cur->childs[*ch - 'a']);
     }
+  }
 
-    // Returns if the word is in the data structure. A word could
-    // contain the dot character '.' to represent any one letter.
-    bool search(string word) {
-      unsigned len = word.size();
-      if( elems.size() < len || elems[len-1] == NULL ) return false;
-      int * hash = elems[len-1];
-      for(unsigned i = 0; i < len; i++){
-        if( word[i] == '.' ) continue;
-        if( hash[i*LEN + word[i] - 'a'] == 0 ) return false;
-      } 
-      return true;
-
-    }
-  
   // private:
-    vector<int *> elems;
+  TrieNode * root;
 };
 
 // Your WordDictionary object will be instantiated and called as such:
@@ -55,11 +63,14 @@ class WordDictionary {
 
 int main() {
   WordDictionary w;
-  w.addWord("pop"); 
-  w.addWord("dad");
-  w.addWord("mad");
+  w.addWord("op"); 
+  w.addWord("ad");
+  w.addWord("ad");
 
-  cout<<w.search("pap")<<endl;
+  cout<<w.search("ap")<<endl;
+  cout<<w.search(".")<<endl;
+  cout<<w.search("o.")<<endl;
+  cout<<w.search(".d")<<endl;
   // Output true, Expected: false !!!
   return 0;
 }
