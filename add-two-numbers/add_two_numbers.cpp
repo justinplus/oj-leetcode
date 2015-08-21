e53b20ff78d747c97da965b17b10b38073d193fc
Justin Jia <justinj656@live.cn>
2015-08-21 10:29:58 +0800

    AC: add-two-numbers.cpp

---
+++ add-two-numbers/add_two_numbers.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+  public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+      int carry = 0;
+      ListNode sum(0); 
+      ListNode *cur = &sum;
+
+      while( l1 && l2) {
+        cur->next = new ListNode( (l1->val + l2->val + carry)%10 );
+        carry = (l1->val + l2->val + carry)/10;
+        cur = cur->next;
+        l1 = l1->next;
+        l2 = l2->next;
+      }
+      l1 = l1 ? l1 : l2;
+      while(l1) {
+        cur->next = new ListNode( (l1->val + carry)%10 );
+        carry = (l1->val + carry)/10;
+        cur = cur->next;
+        l1 = l1->next;
+      }
+      if(carry){
+        cur->next = new ListNode(carry);
+      }
+      return sum.next;
+    }
+};
+
+void inspect( const ListNode * l ) {
+  while(l) {
+    cout<<l->val<< ' ';
+    l=l->next;
+  }
+  cout<<endl;
+}
+
+int main(){
+  ListNode * l1 = new ListNode(3);
+  l1->next = new ListNode(7);
+
+  ListNode * l2 = new ListNode(9);
+  l2->next = new ListNode(2);
+
+  Solution s;
+
+  inspect(s.addTwoNumbers(l1, l2));
+
+  return 0;
+}
+
