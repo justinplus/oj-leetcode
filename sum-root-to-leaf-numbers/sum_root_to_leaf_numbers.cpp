f53fa6ebebfd6a05075929cdff89327c94092fe5
Justin Jia <justinj656@live.cn>
2015-08-26 16:02:52 +0800

    AC: sum_root_to_leaf_numbers.cpp

---
+++ sum-root-to-leaf-numbers/sum_root_to_leaf_numbers.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+  public:
+    int sumNumbers(TreeNode* root) {
+      int sum = 0;
+      vector<TreeNode*> path;
+
+      // using inorder traversal
+      for(; root; path.push_back(root), root = root->left);
+
+      while( path.size() ) {
+        if( path.back()->right ) 
+          for(root = path.back()->right; root; path.push_back(root), root = root->left);
+        else {
+          if(!path.back()->left) sum += rootLeafNumber( path ); // Attention: sum unless path.back()->left ??!!
+          int i = path.size() - 2;
+          for(; i >=0; i--) {
+            if( path[i]->left == path[i+1] ) break;
+          }
+          path.resize(i+1);
+        }
+      }
+      return sum;
+    }
+
+    int rootLeafNumber( const vector<TreeNode*> & v ) {
+      int mul = 1;
+      int res = 0;
+      for(int i = v.size()-1; i >= 0; i--) { // Attention: unsigned -> int, why ??!!
+        res += mul * v[i]->val;
+        mul *= 10;
+      }
+      return res;
+    }
+};
+
+
+int main() {
+  TreeNode * r = new TreeNode(1);
+  r->left = new TreeNode(2);
+  r->right = new TreeNode(3);
+  r->left->left = new TreeNode(1);
+  r->right->left = new TreeNode(0);
+
+  Solution s;
+
+  cout<<s.sumNumbers(r)<<endl;
+  return 0;
+}
