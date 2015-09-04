a522c37c25161af56c092e6478015fa01bd20b91
Justin Jia <justinj656@live.cn>
2015-09-04 14:41:34 +0800

    CPP: use dummy node, add note

--- flatten-binary-tree-to-linked-list/flatten_binary_tree_to_linked_list.cpp
+++ flatten-binary-tree-to-linked-list/flatten_binary_tree_to_linked_list.cpp
@@ -16,13 +16,13 @@ struct TreeNode {
 class Solution { 
   public:
     void flatten(TreeNode* root) {
-      vector<array<TreeNode*, 3>> path;
-      TreeNode *cur, *tmp; // * use before every name
+      TreeNode *cur, *tmp, dummy(0); // Attn: * use before every name
+      vector<array<TreeNode*, 3>> path(1, {&dummy, nullptr, nullptr}); // use dummy node
       array<TreeNode*, 3> nd;
       
       for(; root; path.push_back({root, nullptr, nullptr}), root = root->left);
 
-      while( path.size() ) {
+      while( path.size() > 1 ) {
         cur = path.back()[0];
         if(cur->left) path.push_back({cur->left, nullptr, nullptr});
         else if(cur->right) path.push_back({cur->right, nullptr, nullptr});
@@ -30,6 +30,7 @@ class Solution {
           int i = path.size()-2;
           for( ; i >= 0; i--) {
             nd = path[i+1];
+            path.pop_back();
             if( nd[1] ) {
               nd[1]->right = nd[0]->right;
               nd[0]->right = nd[0]->left;
@@ -38,29 +39,15 @@ class Solution {
             } else {
               tmp = nd[2] ? nd[2] : nd[0];
             }
-            if( path[i][0]->left == path[i+1][0]) {
+            if( path[i][0]->left == nd[0]) {
               path[i][1] = tmp;
               if( path[i][0]->right ){
-                path.pop_back();
                 path.push_back({path[i][0]->right, nullptr, nullptr});
                 break;
-              } else {
-                path.pop_back();
-              }
+              } 
             } else {
               path[i][2] = tmp;
-              path.pop_back();
             }
-          }
-        
-          if(i == -1) {
-            nd = path.back();
-            if( nd[1] ) {
-              nd[1]->right = nd[0]->right;
-              nd[0]->right = nd[0]->left;
-              nd[0]->left = nullptr;
-            }
-            path.pop_back();
           }
         }
       }
@@ -78,16 +65,16 @@ void inspect( TreeNode* r ) {
 int main() {
   TreeNode* r = new TreeNode(1);
   r->left = new TreeNode(2);
-  /* r->right = new TreeNode(5); */
-  // r->left->left = new TreeNode(3);
-  // r->left->right = new TreeNode(4);
-  // r->right->right = new TreeNode(6);
+  r->right = new TreeNode(5); 
+  r->left->left = new TreeNode(3);
+  r->left->right = new TreeNode(4);
+  r->right->right = new TreeNode(6);
 
   Solution s;
   s.flatten(r);
   inspect(r);
   
-  // vector<int[2]> v;
+  // vector<int[2]> v; Attn: Think why the vector<int[2]> do not work here!?
   // v.push_back({1,2});
   // array<int, 2> arr;
   // arr.push_back({1,2});
