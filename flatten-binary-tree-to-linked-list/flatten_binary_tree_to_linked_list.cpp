8a69910a029d0a753cee4a9797be03fd4e76a1b1
Justin Jia <justinj656@live.cn>
2015-09-04 09:50:21 +0800

    AC: flatten_binary_tree_to_linked_list.cpp

---
+++ flatten-binary-tree-to-linked-list/flatten_binary_tree_to_linked_list.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution { 
+  public:
+    void flatten(TreeNode* root) {
+      vector<array<TreeNode*, 3>> path;
+      TreeNode *cur, *tmp; // * use before every name
+      array<TreeNode*, 3> nd;
+      
+      for(; root; path.push_back({root, nullptr, nullptr}), root = root->left);
+
+      while( path.size() ) {
+        cur = path.back()[0];
+        if(cur->left) path.push_back({cur->left, nullptr, nullptr});
+        else if(cur->right) path.push_back({cur->right, nullptr, nullptr});
+        else {
+          int i = path.size()-2;
+          for( ; i >= 0; i--) {
+            nd = path[i+1];
+            if( nd[1] ) {
+              nd[1]->right = nd[0]->right;
+              nd[0]->right = nd[0]->left;
+              nd[0]->left = nullptr;
+              tmp = nd[2] ? nd[2] : nd[1];
+            } else {
+              tmp = nd[2] ? nd[2] : nd[0];
+            }
+            if( path[i][0]->left == path[i+1][0]) {
+              path[i][1] = tmp;
+              if( path[i][0]->right ){
+                path.pop_back();
+                path.push_back({path[i][0]->right, nullptr, nullptr});
+                break;
+              } else {
+                path.pop_back();
+              }
+            } else {
+              path[i][2] = tmp;
+              path.pop_back();
+            }
+          }
+        
+          if(i == -1) {
+            nd = path.back();
+            if( nd[1] ) {
+              nd[1]->right = nd[0]->right;
+              nd[0]->right = nd[0]->left;
+              nd[0]->left = nullptr;
+            }
+            path.pop_back();
+          }
+        }
+      }
+    }
+};
+
+void inspect( TreeNode* r ) {
+  while(r) {
+    cout<<r->val<<' ';
+    r = r->right;
+  }
+  cout<<endl;
+}
+
+int main() {
+  TreeNode* r = new TreeNode(1);
+  r->left = new TreeNode(2);
+  /* r->right = new TreeNode(5); */
+  // r->left->left = new TreeNode(3);
+  // r->left->right = new TreeNode(4);
+  // r->right->right = new TreeNode(6);
+
+  Solution s;
+  s.flatten(r);
+  inspect(r);
+  
+  // vector<int[2]> v;
+  // v.push_back({1,2});
+  // array<int, 2> arr;
+  // arr.push_back({1,2});
+  return 0;
+}
