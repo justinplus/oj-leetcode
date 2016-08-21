1c6df801b544aa6cdbd8c43e3cfea3e475fc0d0d
Justin Jia <justinj656@live.cn>
2016-08-21 13:50:21 +0800

    IMP: reverse-string (java)
    - using char array

--- reverse-string/Solution.java
+++ reverse-string/Solution.java
@@ -1,19 +1,37 @@
 public class Solution {
-  // TODO: improve
+  // Note: char [] is more suitable than StringBuilder here
   public String reverseString(String s) {
-    StringBuilder sb = new StringBuilder(s);
-    int size = sb.length();
+    if( s == null ) return null;
+
+    char[] arr = s.toCharArray();
+    int size = arr.length;
     for( int i = 0; i < size / 2; ++i ) {
-      char ch = sb.charAt(i);
-      sb.setCharAt(i, sb.charAt(size - 1 - i) );
-      sb.setCharAt(size - 1 - i, ch);
+      char ch = arr[i];
+      arr[i] = arr[size - 1 - i];
+      arr[size - 1 - i] = ch;
     }
-    return sb.toString();
+    return new String(arr);
+  }
+
+  public String reverseString1(String s) {
+    if( s == null ) return null;
+
+    char[] arr = s.toCharArray();
+    int bg = 0;
+    int end = arr.length - 1;
+    for( ; bg < end; ++bg, --end ) {
+      char ch = arr[bg];
+      arr[bg] = arr[end];
+      arr[end] = ch;
+    }
+    return new String(arr);
+
   }
 
   public static void main(String[] args) {
     String str = "hello";
     Solution s = new Solution();
     System.out.println(s.reverseString(str));
+    System.out.println(s.reverseString1(str));
   }
 }
