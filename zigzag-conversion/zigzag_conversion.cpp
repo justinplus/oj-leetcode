8d339b6389a549c0255a2e0cc2ceadff83b72888
Justin Jia <justinj656@live.cn>
2016-09-10 16:48:06 +0800

    AC: zigzag_conversion (cpp)
    TODO: check the performance

---
+++ zigzag-conversion/zigzag_conversion.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+  public:
+    string convert(string s, int numRows) {
+      int size = s.size();
+      if(numRows < 2 || numRows >= size) return s;
+      string res;
+      for( int i = 0; i < size; i += 2*numRows - 2 ) res += s[i];
+
+      for( int i = 1; i < numRows-1; ++i) {
+        int d = 0, offset = 0;
+        while( true ) {
+          if( d % 2 == 0 ) {
+            if( offset + i < size )
+              res += s[offset + i];
+            else
+              break;
+            offset += numRows;
+          } else {
+            if( offset + numRows - 2 - i < size )
+              res += s[offset + numRows - 2 - i];
+            else
+              break;
+
+            offset += numRows - 2;
+          }
+          d++;
+        }
+      }
+
+      for( int i = numRows-1; i < size; i += 2*numRows - 2 ) res += s[i];
+      return res;
+    }
+};
+
+int main() {
+  Solution s;
+  cout<<s.convert("PAYPALISHIRING", 3)<<endl;
+  cout<<s.convert("", 3)<<endl;
+  return 0;
+}
